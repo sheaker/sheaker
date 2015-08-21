846eb98d9a20b144cb7fffa0d02ea58619b3e205
pinkmouse <justin.mariette@gmail.com>
2015-08-21 18:07:08 -0500

    Begin remake main page

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -5,8 +5,22 @@
         .module('sheaker')
         .controller('MainController', MainController);
 
-    function MainController() {
-
+    function MainController($scope) {
+        $scope.intervalCarousel = 3500;
+        $scope.noWrapSlides = false;
+        $scope.slides = [
+            {
+                image: '/assets/images/lockupscreen1.png',
+                text1: 'Rogatus ad ultimum admissusque in consistorium ambage'
+            },
+            {
+                image: '/assets/images/lockupscreen2.png',
+                text2: 'Rogatus ad ultimum admissusque in consistorium ambage'
+            },
+            {
+                image: '/assets/images/lockupscreen3.png',
+                text3: 'Rogatus ad ultimum admissusque in consistorium ambage'
+            }
+        ];
     }
-
 })();
