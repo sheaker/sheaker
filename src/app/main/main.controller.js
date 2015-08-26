6b6599c4f8e20a8d140816519a2eb6ddb52aed9e
Tuxity <kevin.darcel@gmail.com>
2015-08-26 18:05:10 -0500

    Translate: prepare variable for main page, cleanup carousel ctrl

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -5,22 +5,24 @@
         .module('sheaker')
         .controller('MainController', MainController);
 
-    function MainController($scope) {
-        $scope.intervalCarousel = 3500;
-        $scope.noWrapSlides = false;
-        $scope.slides = [
-            {
-                image: '/assets/images/lockupscreen1.png',
-                text1: 'Rogatus ad ultimum admissusque in consistorium ambage'
-            },
-            {
-                image: '/assets/images/lockupscreen2.png',
-                text2: 'Lorem ipsum dolor sit amet, ut vel ferri clita interesset'
-            },
-            {
-                image: '/assets/images/lockupscreen3.png',
-                text3: 'Id sale mollis pro, nec iriure numquam in. Ornatus instructior sit te, mel et noster audire iudicabit'
-            }
-        ];
+    function MainController($scope, $translate) {
+        $scope.carousel = {
+            interval:     3500,
+            noWrapSlides: false,
+            slides: [
+                {
+                    image: '/assets/images/lockupscreen1.png',
+                    text1: $translate.instant('MAIN.WELCOME.SLIDES.TEXT_1')
+                },
+                {
+                    image: '/assets/images/lockupscreen2.png',
+                    text2: $translate.instant('MAIN.WELCOME.SLIDES.TEXT_2')
+                },
+                {
+                    image: '/assets/images/lockupscreen3.png',
+                    text3: $translate.instant('MAIN.WELCOME.SLIDES.TEXT_3')
+                }
+            ]
+        };
     }
 })();
